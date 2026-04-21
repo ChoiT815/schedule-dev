783a829f913ac16902613bb734cda519a5ea2585
ChoiT815 <choi081517@gmail.com>
2026-04-21 10:16:44 +0900

    feat : ScheduleRequestDto, ScheduleResponseDto 생성

---
+++ src/main/java/com/example/scheduledev/schedule/dto/ScheduleRequestDto.java
@@ -0,0 +1,23 @@
+package com.example.scheduledev.schedule.dto;
+
+public class ScheduleRequestDto {
+    private String author;
+    private String title;
+    private String content;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
+
+
+
---
+++ src/main/java/com/example/scheduledev/schedule/dto/ScheduleResponseDto.java
@@ -0,0 +1,47 @@
+package com.example.scheduledev.schedule.dto;
+
+import com.example.scheduledev.schedule.domain.Schedule;
+
+import java.time.LocalDateTime;
+
+public class ScheduleResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.title = schedule.getTitle();
+        this.content = schedule.getContent();
+        this.author = schedule.getAuthor();
+        this.createdAt = schedule.getCreatedAt();
+        this.updatedAt = schedule.getUpdatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
