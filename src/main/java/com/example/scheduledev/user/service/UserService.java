4b8507e89a4e512ff376b5f4e61e6a379bd1a8a2
ChoiT815 <choi081517@gmail.com>
2026-04-21 14:47:39 +0900

    feat : UserService 생성

---
+++ src/main/java/com/example/scheduledev/user/service/UserService.java
@@ -0,0 +1,52 @@
+package com.example.scheduledev.user.service;
+
+import com.example.scheduledev.user.domain.User;
+import com.example.scheduledev.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 생성(C)
+    public User createUser(String username, String email) {
+        User user = new User(username, email);
+        return userRepository.save(user);
+    }
+
+    // 전체 조회(R)
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    // 단건 조회(R)
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 일정이 없습니다."));
+    }
+
+    // 수정(U)
+    @Transactional
+    public User updateUser(Long id, String username, String email ) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 일정이 없습니다."));
+        user.update(username, email);
+        return user;
+    }
+
+    // 삭제(D)
+    @Transactional
+    public User deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 일정이 없습니다."));
+        userRepository.delete(user);
+        return user;
+    }
+}
