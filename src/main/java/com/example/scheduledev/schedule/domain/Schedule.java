9a03546e1fc5526592535d962ddd8b02e139af3f
ChoiT815 <choi081517@gmail.com>
2026-04-21 09:28:00 +0900

    feat : Schedule 엔티티 생성 및 JPA Auditing 설정

---
+++ src/main/java/com/example/scheduledev/schedule/domain/Schedule.java
@@ -0,0 +1,65 @@
+package com.example.scheduledev.schedule.domain;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "schedules")
+public class Schedule {
+     //========== 속성 ===========
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 자동 증가
+     private Long id;
+     private String title;
+     private String content;
+     private String password;
+
+     @CreatedDate // 만든 날짜
+     @Column(updatable = false)
+     private LocalDateTime createdAt;
+
+     @LastModifiedDate // 수정 날짜
+     private LocalDateTime updatedAt;
+     private String author;
+
+    //========== 생성자 ===========
+    protected Schedule() {
+
+    }
+
+    public Schedule(String title, String content, String author, String password) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.password = password;
+    }
+
+    //========== Getter ===========
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+}
